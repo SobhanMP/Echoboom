b5b307f871b55943246ff9d8d300843a5276f3cf
Sobhan Mohammadpour <sobhanmohammadpour1@yahoo.fr>
2013-07-19 17:05:20 +0430

    fixed Nextboom.hs compiling error

--- Nextboom.hs
+++ Nextboom.hs
@@ -8,16 +8,17 @@ import Database.HDBC.Sqlite3
 pluginRun :: String -> String
 pluginRun msg =
   if head command == "PING"
-  then "PONG " ++( unwords . tail command)
+  then "PONG " ++ server
   else
     case action of
-      "!add" -> "PRIVMSG " ++ chan ++ ':' ++ adD
-      "!remove" -> "PRIVMSG " ++ chan ++ ':' ++ removE
-      "!read" -> "PRIVMSG " ++ chan ++ ':' ++ reaD
+      "!add" -> "PRIVMSG " ++ chan ++ ":" ++ adD
+      "!remove" -> "PRIVMSG " ++ chan ++ ":" ++ removE
+      "!read" -> "PRIVMSG " ++ chan ++ ":" ++ reaD
       otherwise -> ""
   where command = words msg
-        action = head $ command !! 4
-        chan = command !! 3
+        action = tail $ command !! 4
+        chan =  command !! 3
+        server = unwords . tail $ command
 
 
 reaD = "plugin not complete"
