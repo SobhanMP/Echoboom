83ef277aaeddfa16a45051306bb3a521e82ba169
Sobhan Mohammadpour <sobhanmohammadpour1@yahoo.fr>
2013-07-19 16:33:21 +0430

    added the PRIMSG stuff

--- Nextboom.hs
+++ Nextboom.hs
@@ -1,29 +1,25 @@
+module Nextboom
+(
+  pluginRun)
+where
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
 pluginRun :: String -> String
 pluginRun msg =
   if head command == "PING"
-  then "PONG " ++ unwords . tail command
+  then "PONG " ++( unwords . tail command)
   else
     case action of
-      "!add" -> adD
-      "!remove" -> removE
-      "!read" -> reaD
+      "!add" -> "PRIVMSG " ++ chan ++ ':' ++ adD
+      "!remove" -> "PRIVMSG " ++ chan ++ ':' ++ removE
+      "!read" -> "PRIVMSG " ++ chan ++ ':' ++ reaD
+      otherwise -> ""
   where command = words msg
         action = head $ command !! 4
+        chan = command !! 3
 
 
 reaD = "plugin not complete"
 adD = "plugin not complete"
 removE = "plugin not complete"
---so that we don't get disconnected
-    
-    --  let user = tail . (takeWhile (/=!)) . head command)
---tail to remove the : in the front of the IRC command
---take while to remove the discription sobhan!~~sobhan@2.176.20.194
-      --let chan =  command !! 2
-      --let acction = init $ command !! 3
-      
--- init to remove the first :
-
