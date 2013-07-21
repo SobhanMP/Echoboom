b743e334e6b9a9c8b9d7bacb792722fb9728d97e
Sobhan Mohammadpour <sobhanmohammadpour1@yahoo.fr>
2013-07-21 21:39:54 +0430

    Revert "Fixed a few IO related compile issues"
    This reverts commit 3cbcee8126a50afbf15fe8e268403b2f661598dc.

--- Nextboom.hs
+++ Nextboom.hs
@@ -2,31 +2,22 @@ module Nextboom
 (
   pluginRun)
 where
-
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Control.Monad
-concatIOStrings' :: IO String -> IO String -> IO String
-concatIOStrings' a b = do
-  a' <- a
-  b' <- b
-  return $ a' ++ b'
-concatIOStrings :: [IO String] -> IO String
-concatIOStrings l = foldl concatIOStrings' (return "") l
-
-pluginRun :: (IConnection a) => String -> a -> IO String
+pluginRun :: (IConnection a) => String -> a -> String
 pluginRun msg conn=
   if head command == "PING"
-  then return $ "PONG" ++ server
+  then "PONG " ++ server
   else
     if length command >= 4
     then
       case action of
-        "!add" -> concatIOStrings [return ("PRIVMSG " ++ chan ++ " :" ),adD user message conn]
-        "!remove" -> return $"PRIVMSG " ++ chan ++ " :" ++ removE
-        "!read" -> return $"PRIVMSG " ++ chan ++ " :" ++ reaD
-        otherwise -> return ""
-    else return ""
+        "!add" -> "PRIVMSG " ++ chan ++ " :" ++ adD user message conn 
+        "!remove" -> "PRIVMSG " ++ chan ++ " :" ++ removE 
+        "!read" -> "PRIVMSG " ++ chan ++ " :" ++ reaD 
+        otherwise -> ""
+    else ""
   where command = words msg
         action = tail $ command !! 3
         chan =  command !! 2
@@ -36,13 +27,13 @@ pluginRun msg conn=
 
 
 reaD = "plugin not complete"
-adD :: (IConnection a) => String -> String -> a -> IO String
+adD :: (IConnection a) => String -> String -> a -> String
 adD user message conn = do
   r <- (run conn "INSERT INTO todo VALUES (?,?)" [toSql user,toSql message])
   commit conn
-  if r == 0
-    then return $user++": Sucsess fully added todo"
-    else return $user++": Something has gone wrong :("
+  if ((fromSql r) == 0 )
+    then user++": Sucsess fully added todo"
+    else user++": Something has gone wrong :("
   --where
 --    r<-conn "INSERT INTO todo VALUES (?,?)" [toSql user,toSql msg]) 
 removE = "plugin not complete"
