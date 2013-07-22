0868154ee44edca1b81e4d5173a758b746799618
Sobhan Mohammadpour <sobhanmohammadpour1@yahoo.fr>
2013-07-22 21:23:20 +0430

    adding support for multiple chans

--- irc.hs
+++ irc.hs
@@ -5,10 +5,11 @@ import Nextboom
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Control.Monad
+
 server = "irc.freenode.org"
 port   = 6667
-chan   = "#minix"
-nick   = "echo-boom-bot"
+chan   = ["#echoboom","#minix"]
+nick   = "echo-boom-bot1"
          
 main = do
   conn <- connectSqlite3 "todo.db"
@@ -16,20 +17,23 @@ main = do
   hSetBuffering h NoBuffering
   write h "NICK" nick
   write h "USER" (nick++" 0 * :tutorial bot")
-  write h "JOIN" chan
+  mapM (write h "JOIN") chan
   listen h conn
   disconnect conn
 
 write :: Handle -> String -> String -> IO ()
 write h s t = do
-    hPrintf h "%s %s\r\n" s t
-    printf    "> %s %s\n" s t
+  hPrintf h "%s %s\r\n" s t
+  printf    "> %s %s\n" s t
+write' :: Handle -> String -> IO()
+write' h s = do
+  hPrintf h "%s \r\n" s 
+  printf    "> %s \n" s
 
 listen :: (IConnection a) => Handle -> a ->IO ()
 listen h conn= forever $ do
        s <- hGetLine h
-       let msg = pluginRun s
-       when (msg /= "") $write h "" msg 
-       
+       msg <- (pluginRun s conn)
+       when (msg /= "") $write' h msg       
   where
       forever a = do a; forever a
